ac52c78ec77b8613d8c23ee7c90d34b487976da3
SEM <samerx777222@gmail.com>
2023-11-06 15:10:12 +0100

    Added k8s with configuration for fraud, customer, notifications.

--- customer/src/main/java/com/khomsi/CustomerApplication.java
+++ customer/src/main/java/com/khomsi/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"com.khomsi.customer",
@@ -12,6 +14,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients(
         basePackages = "com.khomsi.clients"
+)
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 )
 public class CustomerApplication {
     public static void main(String[] args) {
